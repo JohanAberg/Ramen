628692ae21b1fb8d8a55587997eed93366d983b8
Esteban Tovagliari <ramenhdr@gmail.com>
2012-05-27 13:40:09 +0200

    Placeholder for node graph editor class.

---
+++ ramen/nodes/node_graph_modifier.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2012 Esteban Tovagliari
+// Licensed under the terms of the CDDL License.
+// See CDDL_LICENSE.txt for a copy of the license.
+
+#include<ramen/nodes/node_graph_modifier.hpp>
+
+namespace ramen
+{
+
+node_graph_modifier_t::node_graph_modifier_t()
+{
+}
+
+} // namespace
---
+++ ramen/nodes/node_graph_modifier.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2012 Esteban Tovagliari
+// Licensed under the terms of the CDDL License.
+// See CDDL_LICENSE.txt for a copy of the license.
+
+#ifndef RAMEN_NODES_NODE_GRAPH_MODIFIER_HPP
+#define RAMEN_NODES_NODE_GRAPH_MODIFIER_HPP
+
+#include<boost/noncopyable.hpp>
+
+namespace ramen
+{
+
+class node_graph_modifier_t : boost::noncopyable
+{
+public:
+
+    node_graph_modifier_t();
+};
+
+} // namespace
+
+#endif
